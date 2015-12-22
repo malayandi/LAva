67f341b18f5cfb45d7074646c689484577897387
Malayandi Palaniappan <malayandi@berkeley.edu>
2015-12-22 17:44:58 +0800

    Reorganized Files
    Deleted “src” folder.
    Stored all java files inside matrix_calculator folder.

--- matrix_calculator/Vector.java
+++ matrix_calculator/Vector.java
@@ -1,39 +1,57 @@
 package matrix_calculator;
 
-import java.util.ArrayList;
-
+/** A class representing the general Vector object.
+ * 
+ * @author Andrew Pau */
 public class Vector {
-    public Vector(int... args) {
-        values = args;
-        numRows = args.length;
-        normalized = new ArrayList<>();
-        int counter = 0;
-        for (int value : values) {
+    public Vector(double... args) {
+        _values = args;
+        _numRows = args.length;
+        _normalization = new double[_numRows];
+        double counter = 0;
+        for (double value : _values) {
             counter += value * value;
         }
-        magnitude = counter;
+        _magnitude = counter;
+        _normalized = false;
     }
 
-    public ArrayList<Double> normalize() {
-        if (!normalized.isEmpty()) {
-            return normalized;
+    /** Returns an array representing the normalized vector. */
+    public double[] normalize() {
+        if (!_normalized) {
+            int count = 0;
+            for (double value : _values) {
+                _normalization[count] = ((double) value / _magnitude);
+                count++;
+            }
+            _normalized = true;
         }
-        for (int value : values) {
-            normalized.add((double) value / magnitude);
-        }
-        return normalized;
+        return _normalization;
     }
 
+    /** Returns the number of entries in this vector. */
     public int numRows() {
-        return numRows;
+        return _numRows;
     }
 
-    public int magnitude() {
-        return magnitude;
+    /** Returns the magnitude of this vector. */
+    public double magnitude() {
+        return _magnitude;
     }
 
-    private int[] values;
-    private int numRows;
-    private int magnitude;
-    private ArrayList<Double> normalized;
+    /** Returns the entries of this vector. */
+    public double[] values() {
+        return _values;
+    }
+
+    /** Boolean indicating whether the vector has been normalized. */
+    private boolean _normalized;
+    /** The values of this vector. */
+    private double[] _values;
+    /** The number of entries in this vector. */
+    private int _numRows;
+    /** The magnitude of this vector. */
+    private double _magnitude;
+    /** The normalized entries of this vector. */
+    private double[] _normalization;
 }
